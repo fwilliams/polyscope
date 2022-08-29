d92895fbcad66615b4f44ebef44fc36ed40f0db8
Nicholas Sharp <nsharp@cs.cmu.edu>
2022-08-29 16:19:53 -0700

    add frameTick() for manual main loop entry

--- include/polyscope/polyscope.h
+++ include/polyscope/polyscope.h
@@ -34,6 +34,9 @@ void init(std::string backend = "");
 // the GUI, possibly by exiting the window.
 void show(size_t forFrames = std::numeric_limits<size_t>::max());
 
+// An alternate method to execute the Polyscope graphical loop. Instead of calling show(), call frameTick() frequently in the user program's loop.
+void frameTick();
+
 // Do shutdown work and de-initialize Polyscope
 void shutdown();
 
